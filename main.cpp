e1754d03e2f78d0bd0cc968e7adc4f3517c978d2
CipollinoVP <yurich2002Vadim@gmail.com>
2022-10-08 06:08:30 +0300

    cm2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,40 @@
 #include "omp.h"
 #include "vector"
 
+std::vector<std::vector<double>> inverse_L(std::vector<std::vector<double>> const& L){
+    std::vector<std::vector<double>> res(L.size());
+    for (int i = 0; i < L.size(); ++i) {
+        res[i] = std::vector<double>(L.size());
+        for (int j = 0; j < L.size(); ++j) {
+            res[i][j] = 0;
+        }
+        res[i][i] = 1;
+    }
+    for (int i = 0; i < L.size()-1; ++i) {
+        for (int j = i+1; j < L.size(); ++j) {
+            for (int k = 0; k < L.size(); ++k) {
+                res[j][k] = res[j][k] - res[i][k]*L[j][i];
+            }
+        }
+    }
+    return res;
+}
+
+std::vector<std::vector<double>> prod(std::vector<std::vector<double>> const& left,
+                                      std::vector<std::vector<double>> const& right){
+    std::vector<std::vector<double>> res(left.size());
+    for (int i = 0; i < left.size(); ++i) {
+        res[i] = std::vector<double>(right[0].size());
+        for (int j = 0; j < right[0].size(); ++j) {
+            res[i][j] = 0;
+            for (int k = 0; k < right.size(); ++k) {
+                res[i][j] += left[i][k]*right[k][j];
+            }
+        }
+    }
+    return res;
+}
+
 void LU(std::vector<std::vector<double>> &A){
     if (A.empty()){
         return;
@@ -25,11 +59,80 @@ void LU(std::vector<std::vector<double>> &A){
 }
 
 void matrix_out(std::vector<std::vector<double>> const& A){
+    if (A.empty()){
+        return;
+    }
+    if (A[0].empty()){
+        return;
+    }
     for (const auto & i : A) {
         for (double j : i) {
             std::cout << j << "  ";
         }
         std::cout << std::endl;
+    }
+}
+
+void LU_Blocks(std::vector<std::vector<double>> &A, int b){
+    for (int i = 0; i < A[0].size()-1; i=i+b) {
+        std::vector<std::vector<double>> subA(A[0].size()-i);
+        for (int j = i; j < A[0].size(); ++j) {
+            subA[j-i] = std::vector<double>(b-1);
+            for (int k = i; k < i+b-1; ++k) {
+                subA[j-i][k-i] = A[j][k];
+            }
+        }
+        LU(subA);
+        for (int j = i; j < A[0].size(); ++j) {
+            for (int k = i; k < i+b-1; ++k) {
+                A[j][k] = subA[j-i][k-i];
+            }
+        }
+        std::vector<std::vector<double>> subL(b-1);
+        for (int j = i; j < i+b-1; ++j) {
+            subL[j-i] = std::vector<double>(b-1);
+            subL[j-i][j-i] = 1;
+            for (int k = j-i+1; k < b-1; ++k) {
+                subL[j-i][k] = 0;
+            }
+            for (int k = i; k < j-i; ++k) {
+                subL[j-i][k-i] = A[j][k];
+            }
+        }
+        subL = inverse_L(subL);
+        subA = std::vector<std::vector<double>>(b-1);
+        for (int j = i; j < i+b-1; ++j) {
+            subA[j-i] = std::vector<double>(A[0].size()-i-b);
+            for (int k = i+b; k < A[0].size(); ++k) {
+                subA[j-i][k-i-b] = A[j][k];
+            }
+        }
+        subA = prod(subL,subA);
+        for (int j = i; j < i+b-1; ++j) {
+            for (int k = i+b; k < A[0].size(); ++k) {
+                 A[j][k] = subA[j-i][k-i];
+            }
+        }
+        std::vector<std::vector<double>> subA1(A[0].size()-i-b);
+        for (int j = i+b; j < A[0].size(); ++j) {
+            subA1[j-i-b] = std::vector<double>(b-1);
+            for (int k = i; k < i+b-1; ++k) {
+                subA1[j-i-b][k-i] = A[j][k];
+            }
+        }
+        std::vector<std::vector<double>> subA2(b-1);
+        for (int j = i; j < i+b-1; ++j) {
+            subA2[j-i] = std::vector<double>(A[0].size()-i-b);
+            for (int k = i+b; k < A[0].size(); ++k) {
+                subA2[j-i][k-i-b] = A[j][k];
+            }
+        }
+        subA = prod(subA1,subA2);
+        for (int j = i+b; j < A[0].size(); ++j) {
+            for (int k = i+b; k < A[0].size(); ++k) {
+                A[j][k] = A[j][k] - subA[j-i-b][k-i-b];
+            }
+        }
     }
 }
 
@@ -47,8 +150,19 @@ int main() {
     }
     std::cout << "Исходная матрица:" << std::endl;
     matrix_out(A);
-    LU(A);
-    std::cout << "После разложения" << std::endl;
+    std::vector<std::vector<double>> B = A;
+    LU(B);
+    std::cout << "После не блочного разложения" << std::endl;
+    matrix_out(B);
+    LU_Blocks(A,55);
+    std::cout << "После блочного разложения" << std::endl;
+    matrix_out(A);
+    for (int i = 0; i < A.size(); ++i) {
+        for (int j = 0; j < A[i].size(); ++j) {
+            A[i][j] = A[i][j] - B[i][j];
+        }
+    }
+    std::cout << "Разность" << std::endl;
     matrix_out(A);
     return 0;
 }
