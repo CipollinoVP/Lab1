ca7a2ed79a90ccbf32463a9587e396e29b7f95b1
CipollinoVP <yurich2002Vadim@gmail.com>
2022-10-14 19:28:29 +0300

    cm2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include "omp.h"
 #include "vector"
+#include "ctime"
 
 std::vector<std::vector<double>> inverse_L(std::vector<std::vector<double>> const& L){
     std::vector<std::vector<double>> res(L.size());
@@ -27,6 +28,22 @@ std::vector<std::vector<double>> prod(std::vector<std::vector<double>> const& le
     std::vector<std::vector<double>> res(left.size());
     for (int i = 0; i < left.size(); ++i) {
         res[i] = std::vector<double>(right[0].size());
+        for (int j = 0; j < right[0].size(); ++j) {
+            res[i][j] = 0;
+            for (int k = 0; k < right.size(); ++k) {
+                res[i][j] = res[i][j] + left[i][k]*right[k][j];
+            }
+        }
+    }
+    return res;
+}
+
+std::vector<std::vector<double>> prod_parallel(std::vector<std::vector<double>> const& left,
+                                      std::vector<std::vector<double>> const& right){
+    std::vector<std::vector<double>> res(left.size());
+    for (int i = 0; i < left.size(); ++i) {
+        res[i] = std::vector<double>(right[0].size());
+#pragma omp parallel for default(none) shared(res,i)
         for (int j = 0; j < right[0].size(); ++j) {
             res[i][j] = 0;
             for (int k = 0; k < right.size(); ++k) {
@@ -232,6 +249,7 @@ int main() {
     int n;
     int m;
     std::cin >> n >> m;
+    m = n;
     std::vector<std::vector<double>> A(n);
     for (auto & i : A) {
         i = std::vector<double>(m);
@@ -239,23 +257,25 @@ int main() {
             j = (double)rand() /RAND_MAX;
         }
     }
-    std::cout << "Исходная матрица:" << std::endl;
-    matrix_out(A);
-    std::vector<std::vector<double>> B = A;
-    LU(B);
-    std::cout << "После не блочного разложения" << std::endl;
-    matrix_out(B);
-    std::cout << "Разница между изначальной и разложением" << std::endl;
-    matrix_out(difference(A,B));
-    check_LU_Blocks(A,32);
-    std::cout << "После блочного разложения" << std::endl;
-    matrix_out(A);
-    for (int i = 0; i < A.size(); ++i) {
-        for (int j = 0; j < A[i].size(); ++j) {
-            A[i][j] = A[i][j] - B[i][j];
+    std::vector<std::vector<double>> B(n);
+    for (auto & i : B) {
+        i = std::vector<double>(m);
+        for (double & j : i) {
+            j = (double)rand() /RAND_MAX;
         }
     }
-    std::cout << "Разность" << std::endl;
-    matrix_out(A);
+    long int t1 = clock();
+    for (int i = 0; i < 1024; ++i) {
+        prod(A,B);
+    }
+    long int t2 = clock();
+    double time1 = (double) (t2-t1)/CLOCKS_PER_SEC;
+    t1 = clock();
+    for (int i = 0; i < 1024; ++i) {
+        prod_parallel(A,B);
+    }
+    t2 = clock();
+    double time2 = (double) (t2-t1)/CLOCKS_PER_SEC;
+    std::cout << time1 << "   " << time2 << std::endl;
     return 0;
 }
