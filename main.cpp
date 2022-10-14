53e81ba82dbfad04ac05bae8bfab3f9764f8266c
CipollinoVP <yurich2002Vadim@gmail.com>
2022-10-14 20:17:49 +0300

    cm2

--- main.cpp
+++ main.cpp
@@ -245,7 +245,7 @@ void check_LU_Blocks(std::vector<std::vector<double>> &A, int b){
 
 int main() {
     omp_set_dynamic(0);
-    omp_set_num_threads(10);
+    omp_set_num_threads(4);
     int a = 5;
     srand(time(0));
     int n;
